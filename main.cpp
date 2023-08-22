112c93ceca91eef797fc7f30960e2fa744a4175a
Christopher Flores <christopher321flores@gmail.com>
2023-08-21 23:22:20 -0400

    Allowing user to choose number of words they want to study

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include "flashCard.h"
 #include <vector>
+#include <fstream>
+#include <string>
 
 int main(int argc, char **argv) {
     //argv[1] = name, argv[2] = method type
     //method type = studying || new || review 
+    if(argc != 3){
+        std::cout << argv[0] << " <Chapter#> <Method>" << std::endl;
+        return 0; 
+    }
     std::string filename(argv[1]);
     filename += ".txt";
     const std::string method(argv[2]);
     FlashCard f{filename};
     //f.print();
-    std::cout << f.get_size() << std::endl;
-    f.guessKanji(10);
+    std::cout << "There are " << f.get_size() << " words " << std::endl << 
+    "how many words you like to study: ";
+    std::string numWords;
+    getline(std::cin,numWords);
+    int getNum = stoi(numWords);
+    while(getNum <= 0 || getNum >= f.get_size()+1){
+        std::cout << "Please enter a valid number: "; 
+        getline(std::cin,numWords);
+        getNum = stoi(numWords);    
+    }
+    f.guessKanji(getNum);
 }
