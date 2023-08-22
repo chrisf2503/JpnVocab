57edac3796ca611c2df7aa72a1d8001b1f32f014
Christopher Flores <christopher321flores@gmail.com>
2023-08-21 22:40:11 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char **argv) {
     //argv[1] = name, argv[2] = method type
     //method type = studying || new || review 
-    const std::string filename(argv[1]);
+    std::string filename(argv[1]);
+    filename += ".txt";
     const std::string method(argv[2]);
     FlashCard f{filename};
     //f.print();
