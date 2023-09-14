674d73b05857bccae3ae75d8fcafa7b1cb369b62
Christopher Flores <christopher321flores@gmail.com>
2023-09-13 22:10:52 -0400

    Allowing user to choose chapter number and lesson number and print those lessons

--- notes.cpp
+++ notes.cpp
@@ -3,18 +3,10 @@
 #include <thread>
 #include <chrono>
 #include <fstream>
-
-int main(int argc, char **argv){
-    if(argc != 2){
-        std::cout << argv[0] << " <Notes#>" << std::endl;
-        return 0;
-    }
-    std::string fileName(argv[1]);
-    fileName += ".md";
-    std::ifstream f(fileName);
+void printAll(std::ifstream &f){
     std::string s;
     while(getline(f,s)){
-        if(s == "Side Note:") {break;}
+
         for(char c: s)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -22,4 +14,57 @@ int main(int argc, char **argv){
         }
         std::cout << '\n';
     }
+}
+//Here we only want to look for the lesson we want to print
+//once its finished, then we want to exit
+void printLesson(std::ifstream & f, const std::string & lesson){
+    std::string s;
+    while(getline(f,s)){
+        if(s.substr(0,7) == lesson){
+            std::cout << s << std::endl;
+            while(getline(f,s)){
+                if(s.substr(0,6) == "Lesson"){
+                    break;
+                }
+                for(char c: s)
+                {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                    std::cout << c << std::flush;
+                }
+                std::cout << '\n';
+                
+            }
+        }
+        break;
+    }
+}
+int main(int argc, char **argv){
+    if(argc != 3){
+        std::cout << argv[0] << " <Chapter#> <Lesson#>" << std::endl;
+        return 0;
+    }
+    std::string fileName(argv[1]);
+    std::string lesson(argv[2]);
+    fileName += ".md";
+    std::ifstream f(fileName);
+    if(!f.is_open()){
+        std::cout << "This current Chapter notes does not exist" << std::endl;
+        f.close();
+        return 0;
+    }
+    int numlesson;
+    f >> numlesson;
+    if(lesson == "All"){
+        printAll(f);
+    }
+    else{
+        //std::cout << "Print the lesson we want" << std::endl;
+        if(stoi(lesson.substr(6)) < 1 || stoi(lesson.substr(6)) > numlesson){
+            std::cout << "This lesson does not exist" << std::endl;
+            f.close();
+            return 0;
+        }
+        printLesson(f,lesson);
+    }
+    f.close();
 }
